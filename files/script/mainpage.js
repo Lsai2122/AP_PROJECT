23afa8accad0623131b6293035826193ad3cd37b
Mouryagna <mouryagnabaindla@gmail.com>
2025-04-06 22:37:10 +0530

    6-4-35 login implementation

---
+++ files/script/mainpage.js
@@ -0,0 +1,28 @@
+document.querySelector('.login').addEventListener('click',()=> {
+    const login=`
+        <div class="block-background">
+            <div class="login-page">
+                <div class="logo">
+                    <img src="../images/logo.png" class="logo-pic">
+                    <p class="logo-name">Eventora</p>
+                </div>
+                <form class="">
+                    <label for="email">Email</label>
+                    <br>
+                    <input type="email" name="email" class="email">
+                    <br>
+                    <label for="password">Password</label>
+                    <br>
+                    <input type="password" name="password" class="password">
+                    <br>
+                    <input type="submit" name="submit" class="login-button" value="Login">
+                </form>
+                <div class="sign-up">
+                    <button class="sign-up-button">Sign Up</button>
+                </div>
+            </div>
+        </div>
+    `;
+    document.querySelector('.login-into').innerHTML=login;
+    document.body.style.overflow = "hidden";
+})
